8df5da13f5f108308944fafc417c98cde9d404af
Emily Holmes <emilyholmes@berkeley.edu>
2022-04-25 17:18:53 -0700

    Message schema

---
+++ backend/models/Message.js
@@ -0,0 +1,29 @@
+const { json } = require("express");
+const mongoose = require("mongoose");
+const User = require("../models/User");
+
+const MessageSchema = mongoose.Schema({
+    messageId: {
+        type: String,
+        required: true,
+    },
+    sender: {
+        type: User,
+        required: true,
+    },
+    receiver: {
+        type: User,
+        required: true,
+    },
+    body: {
+        type: String,
+        required: true,
+    },
+    timestamp: {
+        type: String,
+        required: true,
+    }
+});
+
+// export model user with UserSchema
+module.exports = mongoose.model("message", MessageSchema);
