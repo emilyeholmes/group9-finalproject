aa4631ce0158a6ccf3ff9a3f24db9ec4117e0a44
Emily Holmes <emilyholmes@berkeley.edu>
2022-04-26 22:14:10 -0700

    fixed CORS issue

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,12 @@ const app = express();
 // PORT
 const PORT = process.env.PORT || 4000;
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
+
 // Middleware
 app.use(bodyParser.json());
 
